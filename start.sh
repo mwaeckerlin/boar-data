37a81b9ffaa128df4d6aedbb4cb8e25c3cd20ab8
Marc Wäckerlin <marc@waeckerlin.org>
2016-06-30 22:27:28 +0200

    wrong path

--- start.sh
+++ start.sh
@@ -4,17 +4,18 @@ export SESSIONS=${SESSIONS:-$(boar list | sed -n 's, ([0-9]* revs),,p')}
 
 ! test -e /root/log || rm /root/log;
 ln -sf /dev/stdout /root/log;
-cd /data
 for session in ${SESSIONS}; do
     P=${session##*/}
     P=${P:-$session}
     if ! test -d /data/${P}; then
         echo "$(date) ==== Checkout ${P}"
+        cd /data
         boar co ${session} ${P}
         cd /data/${P}
         echo "$(date) ==== Update ${P}"
         while ! boar update; do "$(date) **** ERROR"; done
     fi
 done
+cd /data
 echo "==== initialized, starting service"
 cron -fL7
