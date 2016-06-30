b6f1e585652972fadcadc0a5d17c440fbd374701
Marc Wäckerlin <marc@waeckerlin.org>
2016-06-30 18:40:05 +0200

    more logs

--- cronjob.sh
+++ cronjob.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+echo "$(date) ==== $0"
 for f in /data/*; do
+    echo "$(date) ---- $f"
     cd $f
     if boar ci -q; then
         boar update -q
@@ -8,4 +10,4 @@ for f in /data/*; do
         boar update -q
         boar ci -q
     fi    
-done
+done 2>&1 >> /root/log
--- start.sh
+++ start.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-if test -z "$SESSIONS"; then
-    echo "set SESSION variable to sessions and pathes to checkout";
-    exit 1;
-fi;
+export SESSIONS=${SESSIONS:-$(boar list | sed -n 's, ([0-9]* revs),,p')}
+
 ! test -e /root/log || rm /root/log;
 ln -sf /dev/stdout /root/log;
 cd /data
@@ -11,7 +9,12 @@ for session in ${SESSIONS}; do
     P=${session##*/}
     P=${P:-$session}
     if ! test -d /data/${P}; then
+        echo "$(date) ==== Checkout ${P}"
         boar co ${session} ${P}
+        cd /data/${P}
+        echo "$(date) ==== Update ${P}"
+        while ! boar update; do "$(date) **** ERROR"; done
     fi
 done
+echo "==== initialized, starting service"
 cron -fL7
